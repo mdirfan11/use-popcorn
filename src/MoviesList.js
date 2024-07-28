5c45fb491305a33841313fc3d91605e563963c9c
Md Irfan <irfan02md@gmail.com>
2024-07-28 12:31:58 +0530

    basic building of all the required components

---
+++ src/MoviesList.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Movie from "./Movie";
+
+function MoviesList({ movies }) {
+  return (
+    <ul className="list">
+      {movies?.map((movie) => (
+        <Movie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+}
+
+export default MoviesList;
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,21 +1,13 @@
-import { useState } from "react";
-import Logo from "./Logo"
+import Logo from "./Logo";
+import Search from "./Search";
 
-function NavBar({ movies }) {
-    const [query, setQuery] = useState("");
+function NavBar({ children }) {
+
     return (
         <nav className="nav-bar">
             <Logo />
-            <input
-            className="search"
-            type="text"
-            placeholder="Search movies..."
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            />
-            <p className="num-results">
-            Found <strong>{movies.length}</strong> results
-            </p>
+            <Search />
+            {children}
         </nav>
     );
 }
